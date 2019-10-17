162081f2270398d117e72ef11184ab1e6cf91595
Helen França <helenfranca93@gmail.com>
2019-10-17 13:20:12 -0300

    Criacao do algoritmo, com algo estranho

--- Genético/genetico.py
+++ Genético/genetico.py
@@ -6,17 +6,26 @@ class Cromossomo:
     def __init__(self, corpo):
         self.corpo = corpo
         
-        # Converte de bin para dec 
-        num = bin2Dec(formata(corpo))
+        # Normaliza
+        dec = bin2Dec(formata(corpo))
+        self.x = (-20) + (20 + 20) * (dec / (pow(2,10) - 1))
         
-        # Calcula a aptidao baseado no numero convertido
-        self.aptidao = math.cos(num) * num + 2
+        # Calcula a aptidao baseado no numero normalizado
+        self.aptidao = math.cos(self.x) * self.x + 2
 
     def get_corpo(self):
         return self.corpo
 
+    def get_x(self):
+        return self.x
+
     def get_aptidao(self):
         return self.aptidao
+    
+    def atualiza(self):
+        dec = bin2Dec(formata(self.corpo))
+        self.x = (-20) + (20 - (-20)) * (dec / pow(2,10) - 1)
+        self.aptidao = math.cos(self.x) * self.x + 2
 
 
 def cria_cromossomo():
@@ -33,6 +42,79 @@ def cria_populacao(numero_populacao):
     ]
     return populacao
 
+def selecao_torneio(populacao):
+    pais = []
+    
+
+    for _ in range(0,len(populacao)):
+        a = random.choice(populacao) # Pega um cromossomo 
+        b = random.choice(populacao) # Pega um cromossomo
+
+    # Verifica quem é menor. O menor se torna um pai para a nova geração
+        if (a.get_aptidao() < b.get_aptidao()):
+            pai = a
+        else:
+            pai = b
+
+        pais.append(pai)
+    
+    return pais
+
+
+def crossover(pais):
+    nova_geracao = []
+    tam = 0
+    while tam < len(pais):
+        taxa_cross = random.uniform(0,1) * 100
+
+        paiUm = pais[tam]
+        paiDois = pais[tam + 1]
+
+        if taxa_cross <= 60 :
+            #Cruzamento
+            corte = random.randrange(1,10)
+            cromossomo_pai_um = paiUm.get_corpo()
+            cromossomo_pai_dois = paiDois.get_corpo()
+
+            filhoUm = cromossomo_pai_um[0:corte] + cromossomo_pai_dois[corte:]
+            filhoDois = cromossomo_pai_dois[0:corte] + cromossomo_pai_um[corte:]
+
+            filhoUm = Cromossomo(filhoUm)
+            filhoDois = Cromossomo(filhoDois)
+            
+        else:
+            filhoUm = paiUm
+            filhoDois = paiDois
+
+        nova_geracao.append(filhoUm)
+        nova_geracao.append(filhoDois) 
+        tam = tam + 2
+
+    return nova_geracao
+
+def mutacao(nova_geracao):
+    for filho in nova_geracao:
+        cromossomo = filho.get_corpo()
+        
+        for i in range(0,len(cromossomo)):
+            taxa_mutacao = random.randrange(0,2)
+            if(taxa_mutacao == 1):
+                if(cromossomo[i] == 0):
+                    cromossomo[i] = 1
+                else:
+                    cromossomo[i] = 0
+        filho.atualiza()
+
+
+def elitismo(nova_geracao, populacao):
+    
+    melhor_pai = sorted(populacao, key=Cromossomo.get_aptidao)[0]
+    filhos_ordenados = sorted(nova_geracao, key=Cromossomo.get_aptidao)
+    melhor_filho = filhos_ordenados[0]
+    
+    if melhor_pai.get_aptidao() < melhor_filho.get_aptidao():
+        nova_geracao[len(nova_geracao) - 1] = melhor_pai
+
 
 def bin2Dec(binary):
 
@@ -45,31 +127,38 @@ def bin2Dec(binary):
     return decimal
 
 
-def formata(a):
+def formata(corpo):
     b = ''
-    for i in a:
+    for i in corpo:
         b = str(i) + '' + b
     return int(b)
 
 
 def main():
 
-	numero_populacao = 5
+    numero_populacao = 10
+    pais = []
+    nova_geracao = []
 
-	# 1° Passo: Criar População e calcular aptidão
-	populacao = cria_populacao(numero_populacao)
+    # 1° Passo: Criar População c/ aptidão
+    populacao = cria_populacao(numero_populacao)
 
+    for _ in range(0,10):
+        # 2° Passo: Seleção por Torneio
+        pais = selecao_torneio(populacao)
+    
+        # 3° Passo: Crossover
+        nova_geracao = crossover(pais)
 
-	# 2° Passo: Seleção por Torneio
-	a = random.choice(populacao).get_aptidao()
-	b = random.choice(populacao).get_aptidao()
-	if (a < b):
-		paiUm = a
-	else:
-		paiUm= b
-	
-    
-  main()
+        # 4° Passo: Mutação
+        mutacao(nova_geracao)
+
+        # 5° Passo: Elitismo
+        elitismo(nova_geracao, populacao)
+        populacao = nova_geracao
+        
+        print(sorted(nova_geracao, key=Cromossomo.get_aptidao)[0].get_aptidao())
     
     
-   
+                        
+main()
