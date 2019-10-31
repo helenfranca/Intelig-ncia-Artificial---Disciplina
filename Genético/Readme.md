9207fff8fa1c03d772248b01b6e368f78e4c07be
Helen França <helenfranca93@gmail.com>
2019-10-31 18:50:42 -0300

    Algoritmo finalizado

--- Genético/Readme.md
+++ Genético/Readme.md
@@ -47,11 +47,11 @@ Utilizar um algoritmo genético binário para minimizar a função descrita abai
   class Cromossomo:
     def __init__(self, corpo):
         self.corpo = corpo
-        
+
         # Normaliza
         dec = bin2Dec(formata(self.corpo))
-        self.x = (-20) + (20 + 20) * (dec / (pow(2,10) - 1))
-        
+        self.x = (-20) + (20 + 20) * (dec / (pow(2, 10) - 1))
+
         # Calcula a aptidao baseado no numero normalizado
         self.aptidao = math.cos(self.x) * self.x + 2
 
@@ -63,23 +63,35 @@ Utilizar um algoritmo genético binário para minimizar a função descrita abai
 
     def get_aptidao(self):
         return self.aptidao
-    
+
     def atualiza(self):
         dec = bin2Dec(formata(self.corpo))
-        self.x = (-20) + (20 + 20) * (dec / (pow(2,10) - 1))
+        self.x = (-20) + (20 + 20) * (dec / (pow(2, 10) - 1))
         self.aptidao = math.cos(self.x) * self.x + 2
+    
+    def mutacao(self):
+        bits = self.get_corpo()
+        for i in range(0, len(bits)):
+            taxa_mutacao = random.uniform(0, 1)
+
+            if(taxa_mutacao <= 0.1):
+                if(bits[i] == 0):
+                    bits[i] = 1
+                else:
+                    bits[i] = 0
+
+        self.corpo = bits
 ```
 
 #### Seleção Torneio
-
+Processo em que se escolhe aleatoriamente dois pais para gerarem filhos
 ```python
   def selecao_torneio(populacao):
     pais = []
-    
-
-    for _ in range(0,len(populacao)):
-        a = random.choice(populacao) # Pega um cromossomo 
-        b = random.choice(populacao) # Pega um cromossomo
+    pop = populacao.copy()
+    for _ in range(0, len(pop)):
+        a = random.choice(pop)  # Pega um cromossomo
+        b = random.choice(pop)  # Pega um cromossomo
 
     # Verifica quem é menor. O menor se torna um pai para a nova geração
         if (a.get_aptidao() < b.get_aptidao()):
@@ -88,86 +100,143 @@ Utilizar um algoritmo genético binário para minimizar a função descrita abai
             pai = b
 
         pais.append(pai)
-    
+
     return pais
 ```
 #### Crossover
+Processo onde acontece o cruzamento dos pais, gerando dois novos filhos. Optamos nesse processo que a acad novo filho gerado ele já passe pelo processo de mutação e atualize sua aptidão. Lembrando que o prórpio cromossomo sabe calcular sua aptidão e também fazer mutação.<br>
+Obs.: Encontramos um melhor resultado quando mudamos a taxa de crossover para 90%.
 
 ```python
-  def crossover(pais):
+  def crossover_mutacao(pais):
     nova_geracao = []
     tam = 0
-    while tam < len(pais):
-        taxa_cross = random.uniform(0,1) * 100
+    pai = pais.copy()
+    while tam < len(pai):
+        taxa_cross = random.uniform(0, 1) * 100
 
-        paiUm = pais[tam]
-        paiDois = pais[tam + 1]
+        paiUm = pai[tam]
+        paiDois = pai[tam + 1]
 
-        if taxa_cross <= 60 :
-            #Cruzamento
-            corte = random.randrange(1,10)
+        if taxa_cross <= 90:
+            # Cruzamento
+            corte = random.randrange(1, 10)
             cromossomo_pai_um = paiUm.get_corpo()
             cromossomo_pai_dois = paiDois.get_corpo()
-
+                      
             filhoUm = cromossomo_pai_um[0:corte] + cromossomo_pai_dois[corte:]
-            filhoDois = cromossomo_pai_dois[0:corte] + cromossomo_pai_um[corte:]
+            filhoDois = cromossomo_pai_dois[0:corte] + \
+                cromossomo_pai_um[corte:]
 
             filhoUm = Cromossomo(filhoUm)
             filhoDois = Cromossomo(filhoDois)
-            
+
         else:
             filhoUm = paiUm
             filhoDois = paiDois
 
+        filhoUm.mutacao()
+        filhoDois.mutacao()
+
+        filhoUm.atualiza()
+        filhoDois.atualiza()
+
         nova_geracao.append(filhoUm)
-        nova_geracao.append(filhoDois) 
+        nova_geracao.append(filhoDois)
         tam = tam + 2
 
     return nova_geracao
 ```
 
-#### Mutação
-
-```python
-  def mutacao(nova_geracao):
-    for filho in nova_geracao:
-        cromossomo = filho.get_corpo()
-        
-        for i in range(0,len(cromossomo)):
-            taxa_mutacao = random.randrange(0,2)
-            if(taxa_mutacao == 1):
-                if(cromossomo[i] == 0):
-                    cromossomo[i] = 1
-                else:
-                    cromossomo[i] = 0
-        filho.atualiza()
-```
-
 #### Elitismo
+Nesse processo busca-se o melhor pai e caso ele seja melhor do que o pior filho, esse filho é substituído.
 
 ```python
   def elitismo(nova_geracao, populacao):
-    
+
     melhor_pai = sorted(populacao, key=Cromossomo.get_aptidao)[0]
-    filhos_ordenados = sorted(nova_geracao, key=Cromossomo.get_aptidao)
-    melhor_filho = filhos_ordenados[0]
-    indice = nova_geracao.index(melhor_filho)
-    
-    if melhor_pai.get_aptidao() < melhor_filho.get_aptidao():
+    filhos_ordenados = sorted(nova_geracao, key=Cromossomo.get_aptidao, reverse=True)
+    pior_filho = filhos_ordenados[0]
+    indice = nova_geracao.index(pior_filho)
+
+    if melhor_pai.get_aptidao() < pior_filho.get_aptidao():
         nova_geracao[indice] = melhor_pai
+    return nova_geracao
+
 ```
 
+#### Programa principal
 
-falta o main()
+```python
+    def main():
 
+        numero_populacao = 10
+        pais = []
+        geracoes = [10,20]
+        
+        for geracao in geracoes:
+            melhores_melhores = []
+            
+            for _ in range(0,geracao):
+
+                # 1° Passo: Criar População c/ aptidão
+                populacao = cria_populacao(numero_populacao)
+                melhores = []
+                cruzado = []   
+                for _ in range(0,numero_populacao):
+                                
+                    # 2° Passo: Seleção por Torneio
+                    pais = selecao_torneio(populacao)
+                
+                    # 3° Passo: Crossover & Mutação & Atualiza
+                    cruzado = crossover_mutacao(pais)      
+                
+                    # 4° Passo: Elitismo
+                    new_geracao = elitismo(cruzado, populacao)
+
+                    populacao = new_geracao
+
+                    melhor_geracao = sorted(populacao, key=Cromossomo.get_aptidao)[0]
+                    melhor_geracao_aptidao = melhor_geracao.get_aptidao()
+                    melhores.append(melhor_geracao_aptidao)
+
+                melhores_melhores.append(melhores)
+
+            escreve_arquivo(conteudo_arquivo(melhores_melhores), geracao)
+
+```
 
 #### Resultados
 
+Abaixo estão os resultados obtidos em cada um dos testes:
 
+![](https://github.com/helenfranca/Inteligencia_Artificial_Disciplina/blob/master/Genetico/Geracao_10.png?raw=true)
+
+![](https://github.com/helenfranca/Inteligencia_Artificial_Disciplina/blob/master/Genetico/Geracao_20.png?raw=true)
+
+Melhor resultado de aptidão para 10 gerações: -16.87594
+Melhor resultado de aptidão para 20 gerações: -16.87594
 
 #### Rodando o algoritmo
 
+##### Importante
 
+É necessário ter Python3 instalado em sua máquina. Caso não tenha, [clique aqui](https://www.python.org/downloads/) e efetue os procedimentos.
+
+- Faça um clone do projeto em sua IDE de preferência ou o download dos arquivos
+- Por meio da linha de comando navegue até o diretório onde se encontram os arquivos-fonte
+
+##### Via linha de comando, escreva:
+
+- python genetico.py
+
+
+##### Lembre-se:
+
+- Caso não possua os arquivos CSV na pasta em questão, o algoritmo irá criá-lo.
+
+---
 ________________
 #### Referências
-Aula AG disponibilizada no AVA
+
+Aula e Slides
--- Genético/genetico.py
+++ Genético/genetico.py
@@ -1,10 +1,5 @@
 import random
 import math
-import numpy as np
-
-import matplotlib
-import matplotlib.pyplot as plt
-
 
 class Cromossomo:
     def __init__(self, corpo):
@@ -30,6 +25,19 @@ class Cromossomo:
         dec = bin2Dec(formata(self.corpo))
         self.x = (-20) + (20 + 20) * (dec / (pow(2, 10) - 1))
         self.aptidao = math.cos(self.x) * self.x + 2
+    
+    def mutacao(self):
+        bits = self.get_corpo()
+        for i in range(0, len(bits)):
+            taxa_mutacao = random.uniform(0, 1)
+
+            if(taxa_mutacao <= 0.1):
+                if(bits[i] == 0):
+                    bits[i] = 1
+                else:
+                    bits[i] = 0
+
+        self.corpo = bits
 
 
 def cria_cromossomo():
@@ -49,10 +57,10 @@ def cria_populacao(numero_populacao):
 
 def selecao_torneio(populacao):
     pais = []
-
-    for _ in range(0, len(populacao)):
-        a = random.choice(populacao)  # Pega um cromossomo
-        b = random.choice(populacao)  # Pega um cromossomo
+    pop = populacao.copy()
+    for _ in range(0, len(pop)):
+        a = random.choice(pop)  # Pega um cromossomo
+        b = random.choice(pop)  # Pega um cromossomo
 
     # Verifica quem é menor. O menor se torna um pai para a nova geração
         if (a.get_aptidao() < b.get_aptidao()):
@@ -65,21 +73,22 @@ def selecao_torneio(populacao):
     return pais
 
 
-def crossover(pais):
+def crossover_mutacao(pais):
     nova_geracao = []
     tam = 0
-    while tam < len(pais):
+    pai = pais.copy()
+    while tam < len(pai):
         taxa_cross = random.uniform(0, 1) * 100
 
-        paiUm = pais[tam]
-        paiDois = pais[tam + 1]
+        paiUm = pai[tam]
+        paiDois = pai[tam + 1]
 
-        if taxa_cross <= 60:
+        if taxa_cross <= 90:
             # Cruzamento
             corte = random.randrange(1, 10)
             cromossomo_pai_um = paiUm.get_corpo()
             cromossomo_pai_dois = paiDois.get_corpo()
-
+                      
             filhoUm = cromossomo_pai_um[0:corte] + cromossomo_pai_dois[corte:]
             filhoDois = cromossomo_pai_dois[0:corte] + \
                 cromossomo_pai_um[corte:]
@@ -91,6 +100,12 @@ def crossover(pais):
             filhoUm = paiUm
             filhoDois = paiDois
 
+        filhoUm.mutacao()
+        filhoDois.mutacao()
+
+        filhoUm.atualiza()
+        filhoDois.atualiza()
+
         nova_geracao.append(filhoUm)
         nova_geracao.append(filhoDois)
         tam = tam + 2
@@ -98,42 +113,12 @@ def crossover(pais):
     return nova_geracao
 
 
-def mutacao(nova_geracao):
-    for filho in nova_geracao:
-        cromossomo = filho.get_corpo()
-
-        for i in range(0, len(cromossomo)):
-            taxa_mutacao = random.randrange(0, 2)
-            if(taxa_mutacao == 1):
-                if(cromossomo[i] == 0):
-                    cromossomo[i] = 1
-                else:
-                    cromossomo[i] = 0
-        filho.atualiza()
-    return nova_geracao
-
-
 def elitismo(nova_geracao, populacao):
-    print('\nELITISMO: ----------')
 
     melhor_pai = sorted(populacao, key=Cromossomo.get_aptidao)[0]
-    filhos_ordenados = sorted(
-        nova_geracao, key=Cromossomo.get_aptidao, reverse=True)
+    filhos_ordenados = sorted(nova_geracao, key=Cromossomo.get_aptidao, reverse=True)
     pior_filho = filhos_ordenados[0]
     indice = nova_geracao.index(pior_filho)
-
-    print("Melhor pai: ", melhor_pai.get_aptidao(),
-          '\nPior filho:', pior_filho.get_aptidao(), '\n')
-
-    # print('PAIS --->', end=" ")
-    # for particula in populacao:
-    #     print(str(round(particula.get_aptidao(), 5)), end=" ")
-    # print()
-
-    # print('FILHOS --->', end=" ")
-    # for particula in nova_geracao:
-    #     print(str(round(particula.get_aptidao(), 5)), end=" ")
-    # print()
 
     if melhor_pai.get_aptidao() < pior_filho.get_aptidao():
         nova_geracao[indice] = melhor_pai
@@ -149,36 +134,18 @@ def escreve_arquivo(texto, geracao):
 def conteudo_arquivo(melhores):
     texto = ''
 
-    texto = '\t;Gen 1; Gen 2; Gen 3;Gen 4; Gen 5; Gen 6;Gen 7; Gen 8; Gen 9; Gen 10 \n Iteracao 1;'
+    texto = '\t;Iter 1; Iter 2; Iter 3; Iter 4; Iter 5; Iter 6; Iter 7; Iter 8; Iter 9; Iter 10 \n Gen 1;'
     j = 1
     for cromossomo in melhores:
         j = j + 1
         for i in range(0, len(cromossomo)):
             # print(cromossomo)
-            texto = texto + \
-                str(round(cromossomo[i], 5)).replace('.', ',') + ';'
-        texto = texto + '\n Iteracao ' + str(j) + ';'
+            texto = texto + str(round(cromossomo[i], 5)).replace('.', ',') + ';'
+        texto = texto + '\n Gen ' + str(j) + ';'
 
     texto += '\n\n'
-    '''
-    texto += 'Média: ;'
-    for item in media_melhores:
-        
-        texto += str(round(item, 5)).replace('.', ',') + ';'
-    '''
+  
     return texto
-
-
-def calcula_media(melhores, geracao):
-    media_melhores = []
-
-    for linha in melhores:
-        somatorio = 0
-        for item in linha:
-            somatorio += item
-        media_melhores.append(somatorio/geracao)
-    return media_melhores
-
 
 def bin2Dec(binary):
 
@@ -204,118 +171,40 @@ def imprimeEnxame(enxame):
     print()
 
 
-def defineMelhor(ultimaLinha):
-
-    menor = ultimaLinha[0]
-    j = 0
-    for i in range(1, len(ultimaLinha)-1):
-        if menor > ultimaLinha[i]:
-            menor = ultimaLinha[i]
-            j = i  # guarda posicao do menor
-    return menor, j
-
-
-def listaMelhorGen(melhores, indice):
-    melhorGen = []
-    for linha in melhores:
-        melhorGen.append(linha[indice])
-    return melhorGen
-
-
 def main():
 
     numero_populacao = 10
     pais = []
-    nova_geracao = []
-
-    geracoes = [10]
-    melhores_melhores = []
-
-    # 1° Passo: Criar População c/ aptidão
-    populacao = cria_populacao(numero_populacao)
-    '''
+    geracoes = [10,20]
+    
     for geracao in geracoes:
         melhores_melhores = []
-    for _ in range(0,10):
-    '''
-    melhores = []
-    for _ in range(0, 10):
-        print("\n /==============================================================/ \n")
-        # 2° Passo: Seleção por Torneio
-        populacao = selecao_torneio(populacao)
-
-        # 3° Passo: Crossover
-        nova_geracao = crossover(populacao)
-
-        # 4° Passo: Mutação
-        nova_geracao = mutacao(nova_geracao)
-
-        imprimeEnxame(populacao)
-        imprimeEnxame(nova_geracao)
-
-        # 5° Passo: Elitismo
-        nova_geracao = elitismo(nova_geracao, populacao)
-
-        populacao = nova_geracao
-
-        # populacao = selecao_torneio(nova_geracao)
-
-        imprimeEnxame(nova_geracao)
-
-        melhor_geracao = sorted(nova_geracao, key=Cromossomo.get_aptidao)[0]
-        melhor_geracao_aptidao = melhor_geracao.get_aptidao()
-        melhores.append(melhor_geracao_aptidao)
-
-    melhores_melhores.append(melhores)
-
-    transposta = np.transpose(melhores_melhores)
-    #  print(transposta, '\n')
-    escreve_arquivo(conteudo_arquivo(transposta), 10)
-
-    # -------------------------------------
-'''
-        print()
-        print("geracao" + str(geracao)+ " :",transposta)
-        print() 
-
-        media = calcula_media(transposta, geracao)
-
-        print()
-
-        # mando ultima linha da matriz de resultado para definicao do menor
-        melhor_Gen = defineMelhor(transposta[-1])
-
-        # print("melhor_Gen: ", melhor_Gen)
-
-        # print("media: ", media)
-
-        print()
         
-        # lista melhor Gen a partir do ultimo melhor
-        lstMelhorGen = listaMelhorGen(transposta, melhor_Gen[1])
+        for _ in range(0,geracao):
 
-        # print("lst melhor Gen: ", lstMelhorGen)
+            # 1° Passo: Criar População c/ aptidão
+            populacao = cria_populacao(numero_populacao)
+            melhores = []
+            cruzado = []   
+            for _ in range(0,numero_populacao):
+                               
+                # 2° Passo: Seleção por Torneio
+                pais = selecao_torneio(populacao)
+            
+                # 3° Passo: Crossover & Mutação & Atualiza
+                cruzado = crossover_mutacao(pais)      
+            
+                # 4° Passo: Elitismo
+                new_geracao = elitismo(cruzado, populacao)
 
+                populacao = new_geracao
 
-        
+                melhor_geracao = sorted(populacao, key=Cromossomo.get_aptidao)[0]
+                melhor_geracao_aptidao = melhor_geracao.get_aptidao()
+                melhores.append(melhor_geracao_aptidao)
 
-        escreve_arquivo(conteudo_arquivo(transposta, media), geracao)
+            melhores_melhores.append(melhores)
 
-        geracao_x_grafico = [x for x in range(1, geracao+1)]
-        
-        # grafico
-        fig, ax = plt.subplots()
-        color = 'tab:red'
-        plt.plot(geracao_x_grafico, media, color=color, label='Média')
-        color = 'tab:blue'
-        plt.xlabel('Geração')
-        plt.ylabel('Aptidão')
-        plt.plot(geracao_x_grafico, lstMelhorGen, color=color, label='Melhor Gen')
-        plt.grid(True)
-        plt.legend(loc=0)
-        plt.savefig('graficoGeracao_'+str(geracao)+'.png')
-
-'''
-
+        escreve_arquivo(conteudo_arquivo(melhores_melhores), geracao)
 
 main()
